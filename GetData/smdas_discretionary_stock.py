fb5d154d8113def73bbbcf178d61a5df4d075404
nizhe <805876838@qq.com>
2023-07-10 16:14:59 +0800

    v1.1:smdas_stock

--- GetData/smdas_discretionary_stock.py
+++ GetData/smdas_discretionary_stock.py
@@ -13,92 +13,105 @@ from sqlalchemy.orm import column_property, relationship, deferred
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-config = dotenv_values(".env")
-host = config["host"]
-port = config["port"]
-user = config["user"]
-passwd = config["passwd"]
-db = config["db3"]
-connect_timeout = config["connect_timeout"]
+class SmdasDiscretionaryStock:
 
-db_url = "".join(["mysql+pymysql://", user, ":", passwd, "@", host, ":", port, "/", db, "?charset=utf8"])
-engine = create_engine(db_url)
-Base = declarative_base()
-Session = sessionmaker(engine)
+    def __init__(self):
+        config = dotenv_values(".env")
+        host = config["host"]
+        port = config["port"]
+        user = config["user"]
+        passwd = config["passwd"]
+        db = config["db3"]
+        connect_timeout = config["connect_timeout"]
 
+        db_url = "".join(["mysql+pymysql://", user, ":", passwd, "@", host, ":", port, "/", db, "?charset=utf8"])
+        self.engine = create_engine(db_url)
+        self.Base = declarative_base()
 
-# class the table
-class discretionary_stock(Base):
-    __tablename__ = 'discretionary_stock'
-    id = Column(int, primary_key=True)
-    account = Column(str)
-    stock_code = Column(str)
+        Session = sessionmaker(engine)
+        self.session = Session()
 
 
-class personality_module(Base):
-    __tablename__ = 'discretionary_stock'
-    id = Column(int, primary_key=True)
-    account = Column(str)
-    preference = Column(str)
-    prediction = Column(str)
-    history = Column(str)
+    def table_create(self):
+    # class the table
+        class discretionary_stock(self.Base):
+            __tablename__ = 'discretionary_stock'
+            id = Column(int, primary_key=True)
+            account = Column(str)
+            stock_code = Column(str)
 
 
-# create the record
-def create_discretionary_stock(self):
-    with Session() as session:
-        discretionary = discretionary_stock(
-
-        )
-        Session.add(discretionary_stock)
-        Session.commit()
-
-
-def create_personality_module(self):
-    with Session() as session:
-        personality = personality_module(
-
-        )
-        Session.add(personality_module)
-        Session.commit()
+        class personality_module(self.Base):
+            __tablename__ = 'discretionary_stock'
+            id = Column(int, primary_key=True)
+            account = Column(str)
+            preference = Column(str)
+            prediction = Column(str)
+            history = Column(str)
 
 
-# modify the record
-def modify_discretionary_stock(QueryCriteria, Target):
-    with Session() as session:
-        discretionary = Session.query(discretionary_stock).filter_by(QueryCriteria).first()
-        discretionary.attribute = 'Target'
-        Session.commit()
+    def record_create(self):
+    # create the record
+        def create_discretionary_stock(self, account1, stockcode1):
+            with Session() as session:
+                discretionary = discretionary_stock(
+                    account = account1, stockcode = stockcode1
+                )
+                Session.add(discretionary_stock)
+                Session.commit()
 
 
-def modify_personality_module(QueryCriteria, Target):
-    with Session() as session:
-        personality = Session.query(personality_module).filter_by(QueryCriteria).first()
-        personality.attribute = 'Target'
-        Session.commit()
+        def create_personality_module(self, account2, preference2, prediction2, history2):
+            with Session() as session:
+                personality = personality_module(
+                    account = account2, preference = preference2,
+                    prediction = prediction2, history = history2
+                )
+                Session.add(personality_module)
+                Session.commit()
 
 
-# delete the record
-def delete_discretionary_stock(QueryCriteria):
-    with Session() as session:
-        discretionary = Session.query(discretionary_stock).filter_by(QueryCriteria).first()
-        Session.delete(discretionary)
-        Session.commit()
+    # modify the record
+    def record_modify(self):
+        def modify_discretionary_stock(self, keyword, condition, Target):
+            with Session() as session:
+                discretionary = Session.query(discretionary_stock).filter_by(keyword == condition).first()
+                discretionary.attribute = Target
+                Session.commit()
 
 
-def delete_personality_module(QueryCriteria):
-    with Session() as session:
-        personality = Session.query(personality_module).filter_by(QueryCriteria).first()
-        Session.delete(personality)
-        Session.commit()
+        def modify_personality_module(self, keyword, condition, Target):
+            with Session() as session:
+                personality = Session.query(personality_module).filter_by(keyword == condition).first()
+                personality.attribute = Target
+                Session.commit()
 
 
-# basic query
-def discretionary_stock_query(keywordcondition):
-    with Session() as session:
-        discretionary = Session.query(discretionary_stock).filter(keywordcondition).all()
+    # delete the record
+    def record_delete(self):
+        def delete_discretionary_stock(self, keyword, condition):
+            with Session() as session:
+                discretionary = Session.query(discretionary_stock).filter_by(discretionary_stock.keyword == condition).first()
+                Session.delete(discretionary)
+                Session.commit()
 
 
-def personality_module_query(keywordcondition):
-    with Session() as session:
-        personality = Session.query(personality_module).filter(keywordcondition).all()
+        def delete_personality_module(self, keyword, condition):
+            with Session() as session:
+                personality = Session.query(personality_module).filter_by(personality_module.keyword == condition).first()
+                Session.delete(personality)
+                Session.commit()
+
+
+    # basic query
+    def info_query(self):
+        def discretionary_stock_query(self, keyword, condition):
+            with Session() as session:
+                discretionary = Session.query(discretionary_stock).filter(discretionary_stock.keyword == condition).all()
+                data = [dict(zip(discretionary.keys(),discretionary)) for results in discretionary]
+
+
+        def personality_module_query(self, keyword, condition):
+            with Session() as session:
+                personality = Session.query(personality_module).filter(personality_module.keyword == condition).all()
+                data = [dict(zip(personality.keys(),personality)) for result in personality]
--- GetData/smdas_user.py
+++ GetData/smdas_user.py
@@ -13,55 +13,64 @@ from sqlalchemy.orm import column_property, relationship, deferred
 from sqlalchemy.orm import declarative_base
 from sqlalchemy.orm import sessionmaker
 
-config = dotenv_values(".env")
+class SmdasUser:
+    def __init__(self):
+        config = dotenv_values(".env")
 
-host=config["host"]
-port=config["port"]
-user=config["user"]
-passwd=config["passwd"]
-db=config["db1"]
-connect_timeout=config["connect_timeout"]
+        host=config["host"]
+        port=config["port"]
+        user=config["user"]
+        passwd=config["passwd"]
+        db=config["db1"]
+        connect_timeout=config["connect_timeout"]
 
-db_url = "".join(["mariadb+mariadbconnector://", user, ":", passwd, "@", host, ":", port, "/", db, "?charset=utf8"])
-engine = create_engine(db_url)
+        db_url = "".join(["mariadb+mariadbconnector://", user, ":", passwd, "@", host, ":", port, "/", db, "?charset=utf8"])
+        self.engine = create_engine(db_url)
+        self.Base = declarative_base()
 
-Base = declarative_base()
+        Session = sessionmaker(bind = self.engine)
+        self.session = Session()
 
-#class the table
-class info(Base):
-    __tablename__ = 'info'
-    id = Column(int,primary_key=True)
-    account = Column(str)
-    password = Column(str)
-    identity = Column(bool)
-    question = Column(str)
-    answer = Column(str)
-    nickname = Column(str)
+    def table_create(self):
+        #class the table
+        class info(self.Base):
+            __tablename__ = 'info'
+            id = Column(int,primary_key=True)
+            account = Column(str)
+            password = Column(str)
+            identity = Column(bool)
+            question = Column(str)
+            answer = Column(str)
+            nickname = Column(str)
 
-Session = sessionmaker(bind=engine)
-Session = Session()
+    def record_create(self):
+        #create the record
+        def create_user(self, account1, password1, identity1, question1, answer1, nickname1):
+            information = info(
+                account = account1, password = password1, identity = identity1,
+                question = question1, answer = answer1, nickname = nickname1
+            )
+            Session.add(information)
+            Session.commit()
 
-#create the record
-def create_user(self):
-    Info = info(
+    def record_modify(self):
+        #modify the record
+        def modify_user(self, keyword, condition, Target):
+            information = Session.query(info).filter_by(info.keyword == condition).first()
+            information.attribute = Target
+            Session.commit()
 
-    )
-    Session.add(Info)
-    Session.commit()
+    def record_delete(self):
+        #delete the record
+        def delete_user(self, keyword, condition):
+            information = Session.query(info).filter_by(info.keyword == condition).first()
+            Session.delete(information)
+            Session.commit()
 
-#modify the record
-def modify_user(QueryCriteria):
-    Info = Session.query(info).filter_by(QueryCriteria).first()
-    Info.attribute = 'Target'
-    Session.commit()
+    def info_query(self):
+        #basic query
+        def info_query(self, keyword, condition):
+            information = Session.query(info).filter(info.keyword == condition).all()
+            data = [dict(zip(information.keys(), information)) for result in information]
 
-#delete the record
-def delete_user(QueryCriteria):
-    Info = Session.query(info).filter_by(QueryCriteria).first()
-    Session.delete(Info)
-    Session.commit()
-
-#basic query
-def info_query(keywordcondition):
-    Info = Session.query(info).filter(keywordcondition).all()
 
